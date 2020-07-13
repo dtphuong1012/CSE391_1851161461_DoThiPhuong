fe3f993cdb42da758baf8a96e2e8f5fc6992db97
dtphuong1012 <67354366+dtphuong1012@users.noreply.github.com>
2020-07-13 21:29:46 +0700

    sửa BTTT_04

--- BTTH_04/script.js
+++ BTTH_04/script.js
@@ -1,7 +1,7 @@
 //xacs dinh cac ptu bi tac dong
 
 var jFruit     =document.getElementById('txtFruit');
-var jShowFruit =document.getElementById('showFruit');
+var jShowFruit =document.getElementById('btnShowFruit');
 var jImgFruit  =document.getElementById('imgFruit');
 
 //bat su kien
@@ -11,3 +11,11 @@ function showResult (){
     var getFruit =jFruit.value;
     jImgFruit.setAttribute('src',getFruit+'.jpeg')
 }
+//jquery cu phap: $(selector).Action() or jquery(selector).action()
+// vd
+// $(document).ready(function(){
+//     $("#btnShowfruit").clck(function(){
+//         let getFruit=$("txtFruit").val() ;
+//         $("#imgFruit").attr('src',getFruit+'jpeg');
+//     })
+// })
